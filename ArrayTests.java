338c85f7f188ab84263a6c461db12e15f19f8220
Jason Huang <jasonhuang@Jasons-MacBook-Air.local>
2024-01-31 11:14:35 -0800

    update

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import java.lang.reflect.InaccessibleObjectException;
 import org.junit.*;
 
 public class ArrayTests {
@@ -7,6 +8,10 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = { 1, 2, 3 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] { 3, 2, 1}, input2);
 	}
 
 
@@ -14,5 +19,8 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = { 1, 2, 3};
+    assertArrayEquals(new int[] { 3, 2, 1 }, ArrayExamples.reversed(input2));
   }
 }
